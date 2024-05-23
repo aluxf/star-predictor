d0b747afade8dbb0a3cf11155a06c250016a31ee
aluxf <79197205+aluxf@users.noreply.github.com>
2024-05-23 14:36:46 +0200

    Force push to dev

--- dev/train.py
+++ dev/train.py
@@ -4,7 +4,7 @@ def train():
     with open('best_model.pt', 'w') as f:
         f.write('best model')
     with open('github.pt','w') as f:
-        f.write('github actions works!!!!')
+        f.write('github actions works!!!!!')
 
 if __name__ == '__main__':
     train()
