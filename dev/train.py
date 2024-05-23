48cb2c532aba0fc3302f38e7fa76b73a701d6e2d
aluxf <alexfooladi@hotmail.com>
2024-05-23 12:53:43 +0000

    Create git user on dev

--- dev/train.py
+++ dev/train.py
@@ -2,7 +2,7 @@ import sklearn
 
 def train():
     with open('best_model.pt', 'w') as f:
-        f.write('best model')
+        f.write('best model!!')
     with open('github.pt','w') as f:
         f.write('github actions works!!!!!')
 
