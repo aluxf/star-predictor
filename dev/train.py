b3cbe2a628f1e40512dfcb680981d02a01129f82
aluxf <alexfooladi@hotmail.com>
2024-05-23 13:48:18 +0000

    Update permissions on remote gits

--- dev/train.py
+++ dev/train.py
@@ -2,7 +2,7 @@ import sklearn
 
 def train():
     with open('best_model.pt', 'w') as f:
-        f.write('best model!! working!')
+        f.write('best model!! working!!')
     with open('github.pt','w') as f:
         f.write('github actions works!!!!!')
 
