d17c669d163a99adfa219bb6ab2d89e28744daeb
aluxf <alexfooladi@hotmail.com>
2024-05-27 16:13:12 +0200

    dockerize ansible

---
+++ ansible/env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Source the .sh file
+source opensrc.sh
+
+# Export the environment variables to a .env file
+echo "OS_USER_DOMAIN_NAME=$OS_USER_DOMAIN_NAME" >> .env
+echo "OS_PROJECT_DOMAIN_ID=$OS_PROJECT_DOMAIN_ID" >> .env
+echo "OS_USERNAME=$OS_USERNAME" >> .env
+echo "OS_PASSWORD=$OS_PASSWORD" >> .env
+echo "OS_REGION_NAME=$OS_REGION_NAME" >> .env
+echo "OS_INTERFACE=$OS_INTERFACE" >> .env
+echo "OS_IDENTITY_API_VERSION=$OS_IDENTITY_API_VERSION" >> .env
