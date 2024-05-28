7b7125fbc1145d7266d50953233c69ab058d6c1b
aluxf <alexfooladi@hotmail.com>
2024-05-28 11:24:22 +0000

    ignore cluster launcher

--- vm/ansible/ansible.sh
+++ vm/ansible/ansible.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i hosts ssh-playbook.yml --private-key=/.ssh/cluster-key
-ansible-playbook -i hosts setup-playbook.yml --private-key=.ssh/cluster-key
+ansible-playbook -i hosts setup-playbook.yml --private-key=/.ssh/cluster-key
